cfae840ecd5fb6c619101ce5402dcc04d9f70a71
ExtincT <marklestercomia22@gmail.com>
2025-08-24 17:33:06 +0800

    secret

--- bank_mysql.py
+++ bank_mysql.py
@@ -5,7 +5,7 @@ from datetime import datetime
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Nightdespair22!",
+    password="********",  #hulaan mo
     database="bank_app"
 )
 cursor = conn.cursor()
